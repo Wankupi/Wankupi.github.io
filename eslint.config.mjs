53f8cce096e46ed65f27feb22ff6e7e23d90a17d
Wankupi <2893353848@qq.com>
2025-05-09 22:40:40 -0400

    feat(main): support eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { ignores: ["**/.cache/**", "**/node_modules/**", "**/dist/**", "**/.temp/**"] },
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+]);
